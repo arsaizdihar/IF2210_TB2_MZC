aa1ae31d38fb896281293f34f1c8483204bae27e
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-05-04 13:14:10 +0700

    customer selector (add missed file)

--- app/src/main/java/mzc/app/view_model/components/cashier/PaymentSummaryViewModel.java
+++ app/src/main/java/mzc/app/view_model/components/cashier/PaymentSummaryViewModel.java
@@ -1,6 +1,20 @@
 package mzc.app.view_model.components.cashier;
 
+import javafx.scene.layout.VBox;
+import lombok.Getter;
+import mzc.app.view.components.cashier.CustomerSelectorView;
 import mzc.app.view_model.components.split_page.RightSideViewModel;
 
 public class PaymentSummaryViewModel extends RightSideViewModel {
+    @Getter
+    private CustomerSelectorView customerSelector = new CustomerSelectorView();
+
+    @Getter
+    private VBox container = new VBox();
+
+    @Override
+    public void init() {
+        super.init();
+        createView(this.customerSelector);
+    }
 }
