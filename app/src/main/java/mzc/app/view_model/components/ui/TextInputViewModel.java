9aeb9389ad0e85520cdbb5495f915b374f4002bf
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-05-05 22:11:07 +0700

    feat: add number only to textfield

--- app/src/main/java/mzc/app/view_model/components/ui/TextInputViewModel.java
+++ app/src/main/java/mzc/app/view_model/components/ui/TextInputViewModel.java
@@ -1,5 +1,7 @@
 package mzc.app.view_model.components.ui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import lombok.Getter;
@@ -19,6 +21,8 @@ public class TextInputViewModel extends BaseViewModel {
     public void init() {
         this.main.getChildren().add(this.label);
         this.main.getChildren().add(this.textField);
+
+
     }
 
     public void setter(String str, int width) {
@@ -26,4 +30,24 @@ public class TextInputViewModel extends BaseViewModel {
         this.textField.setPromptText(str);
         this.textField.setPrefWidth(width);
     }
+
+    public String getVal() {
+        return this.textField.getText();
+    }
+
+    public void numberOnly(boolean bool) {
+
+        if (bool) {
+            textField.textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                    String newValue) {
+                    if (!newValue.matches("\\d*")) {
+                        textField.setText(newValue.replaceAll("[^\\d]", ""));
+                    }
+                }
+            });
+        }
+    }
+
 }
