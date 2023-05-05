5e0115e66b00f9b4cf57f7b0b309968a04250967
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-05-05 22:12:52 +0700

    feat: update textinput and save Product model

--- app/src/main/java/mzc/app/view_model/components/product_list/RightSideProductViewModel.java
+++ app/src/main/java/mzc/app/view_model/components/product_list/RightSideProductViewModel.java
@@ -1,11 +1,10 @@
 package mzc.app.view_model.components.product_list;
 
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -16,10 +15,13 @@ import mzc.app.view.components.FileDialogView;
 import mzc.app.view.components.ui.TextInputView;
 import mzc.app.view_model.components.split_page.RightSideViewModel;
 import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
 
+import java.io.Console;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Objects;
 
 @Getter
@@ -28,6 +30,16 @@ public class RightSideProductViewModel extends RightSideViewModel {
     private HBox mainCol;
     private VBox list;
     private VBox image = new VBox();
+    private Button kirim;
+
+    private TextInputView namaBarang;
+    private TextInputView hargaBeli;
+    private TextInputView hargaJual;
+    private TextInputView stok;
+
+    private ComboBox<String> kategoriField;
+    private Image imageFile;
+    private String imagePath;
 
     @Override
     public void init() {
@@ -37,10 +49,10 @@ public class RightSideProductViewModel extends RightSideViewModel {
         tambahBarang.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
         setupLines();
         setupImage();
-        Button kirim = new Button("Kirim");
-        kirim.setPrefWidth(100);
-        kirim.getStyleClass().addAll("btn", "btn-success");
-        kirim.setStyle("-fx-font-weight: bold;");
+        this.kirim = new Button("Kirim");
+        this.kirim.setPrefWidth(100);
+        this.kirim.getStyleClass().addAll("btn", "btn-success");
+        this.kirim.setStyle("-fx-font-weight: bold;");
         this.mainCol = new HBox(this.list, this.image);
         this.mainCol.setAlignment(Pos.CENTER);
         this.mainCol.setSpacing(100);
@@ -48,25 +60,31 @@ public class RightSideProductViewModel extends RightSideViewModel {
         this.main.setAlignment(Pos.CENTER);
         this.main.setSpacing(100);
         this.main.setStyle("-fx-font-size: 16px;");
+
+        setOnButtonClicked((e) -> {
+            Product product = new Product(Integer.parseInt(this.stok.getViewModel().getVal()), this.namaBarang.getViewModel().getVal(), BigDecimal.valueOf(Integer.parseInt(this.hargaJual.getViewModel().getVal())), BigDecimal.valueOf(Integer.parseInt(this.hargaBeli.getViewModel().getVal())), this.kategoriField.getValue(), this.imagePath);
+            getAdapter().getProduct().persist(product);
+            System.out.println("Saved!");
+        });
     }
 
     private void setupLines() {
-        TextInputView namaBarang = new TextInputView("Nama Barang", 200);
+        namaBarang = new TextInputView("Nama Barang", 200, false);
         createView(namaBarang);
         Label kategori = new Label("Kategori");
 
-        ComboBox<String> kategoriField = new ComboBox<>(FXCollections.observableArrayList(
+        kategoriField = new ComboBox<>(FXCollections.observableArrayList(
                 "Option 1",
                 "Option 2",
                 "Option 3"
         ));
         kategoriField.setPrefWidth(200);
         VBox kat = new VBox(kategori, kategoriField);
-        TextInputView hargaBeli = new TextInputView("Harga Beli", 200);
+        hargaBeli = new TextInputView("Harga Beli", 200, true);
         createView(hargaBeli);
-        TextInputView hargaJual = new TextInputView("Harga Jual", 200);
+        hargaJual = new TextInputView("Harga Jual", 200, true);
         createView(hargaJual);
-        TextInputView stok = new TextInputView("Stok", 200);
+        stok = new TextInputView("Stok", 200, true);
         createView(stok);
         this.list = new VBox(namaBarang.getView(), kat, hargaBeli.getView(), hargaJual.getView(), stok.getView());
         this.list.setSpacing(15);
@@ -79,27 +97,39 @@ public class RightSideProductViewModel extends RightSideViewModel {
         imageView.setFitHeight(100);
         this.image.getChildren().add(imageView);
         Button pilihGambar = new Button("Pilih Gambar");
-        Label error = new Label("File tidak valid");
         var fileDialogView = new FileDialogView(pilihGambar, file -> {
             try {
-                Image image = new Image(new FileInputStream(file));
-                imageView.setImage(image);
+                imageFile = new Image(new FileInputStream(file));
+                imageView.setImage(imageFile);
+                System.out.println(imageFile);
             } catch (FileNotFoundException e) {
-                this.image.getChildren().add(error);
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("File Error");
+                alert.setHeaderText("File is not found or is not valid");
+                alert.setContentText("Pastikan file yang dipilih merupakan file gambar yang valid");
+
+                alert.showAndWait();
             }
-            String absolutePath = file.getAbsolutePath();
+            this.imagePath = file.getAbsolutePath();
             Product product = new Product();
             try {
-                product.updateImage(absolutePath);
+                product.updateImage(this.imagePath);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        });
+        }, "Images (*.png, *.jpg, *.jpeg)", "*.png", "*.jpg", ".jpeg");
         createView(fileDialogView);
         pilihGambar.getStyleClass().add("btn");
 
 
         this.image.getChildren().add(fileDialogView.getView());
+    }
+
+    public void setOnButtonClicked(EventHandler<ActionEvent> handler) {
+        if (this.kirim == null) {
+            throw new RuntimeException("Must set button first");
+        }
+        this.kirim.setOnAction(handler);
     }
 }
 
