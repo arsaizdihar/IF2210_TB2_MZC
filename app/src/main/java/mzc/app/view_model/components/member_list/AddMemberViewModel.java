e1697f54bf9b0a3b209da5056b5d05002e21214b
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-05-06 03:01:10 +0700

    refactor: refactor addmemberView

--- app/src/main/java/mzc/app/view_model/components/member_list/AddMemberViewModel.java
+++ app/src/main/java/mzc/app/view_model/components/member_list/AddMemberViewModel.java
@@ -1,19 +1,16 @@
 package mzc.app.view_model.components.member_list;
 
-import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import lombok.Getter;
-import mzc.app.annotation.ModelInject;
+import mzc.app.model.Customer;
+import mzc.app.model.CustomerType;
 import mzc.app.view.components.ui.TextInputView;
 import mzc.app.view_model.components.split_page.RightSideViewModel;
-import mzc.app.view_model.components.ui.TextInputViewModel;
-import org.jetbrains.annotations.NotNull;
 
 @Getter
 public class AddMemberViewModel extends RightSideViewModel {
@@ -23,42 +20,56 @@ public class AddMemberViewModel extends RightSideViewModel {
     private HBox mainCol;
     @Getter
     private VBox list;
-
+    @Getter private  TextInputView name = new TextInputView("Nama", 200, false);
+    @Getter private TextInputView phoneNumber = new TextInputView("Nomor Handphone", 200, true);
+    @Getter private ComboBox<CustomerType> categoryField = new ComboBox<>();
+    @Getter private VBox kat;
+    @Getter private Button addCustomerButton;
     @Override
     public void init() {
         super.init();
         Label tambahBarang = new Label("Tambah Anggota");
         tambahBarang.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
         setupLines();
-        Button kirim = new Button("Kirim");
-        kirim.setPrefWidth(100);
-        kirim.getStyleClass().addAll("btn", "btn-success");
-        kirim.setStyle("-fx-font-weight: bold;");
+        addCustomerButton = new Button("Kirim");
+        addCustomerButton.setOnAction(event -> {
+            if (name.getViewModel().getTextField().getText().isEmpty() || phoneNumber.getViewModel().getTextField().getText().isEmpty() || categoryField.getValue() == null) {
+                System.out.println("Tidak boleh kosong");
+            } else {
+                Customer customer = new Customer();
+                customer.setName(name.getViewModel().getTextField().getText());
+                customer.setPhone(phoneNumber.getViewModel().getTextField().getText());
+                customer.setType(categoryField.getValue());
+                getAdapter().getCustomer().persist(customer);
+                System.out.println("Nama: " + name.getViewModel().getTextField().getText());
+                System.out.println("Nomor Handphone: " + phoneNumber.getViewModel().getTextField().getText());
+                System.out.println("Kategori: " + categoryField.getValue());
+            }
+        });
+        addCustomerButton.setPrefWidth(100);
+        addCustomerButton.getStyleClass().addAll("btn", "btn-success");
+        addCustomerButton.setStyle("-fx-font-weight: bold;");
         this.mainCol = new HBox(this.list);
         this.mainCol.setAlignment(Pos.CENTER);
         this.mainCol.setSpacing(100);
-        this.main = new VBox(tambahBarang, this.mainCol, kirim);
+        this.main = new VBox(tambahBarang, this.mainCol, addCustomerButton);
         this.main.setAlignment(Pos.CENTER);
         this.main.setSpacing(100);
         this.main.setStyle("-fx-font-size: 16px;");
     }
 
     private void setupLines() {
-        TextInputView nama = new TextInputView("Nama", 200, false);
-        createView(nama);
-        TextInputView nomorHandphone = new TextInputView("Nomor Handphone", 200, false);
-        createView(nomorHandphone);
+        createView(name);
+        createView(phoneNumber);
         Label kategori = new Label("Tipe Anggota");
 
-        ComboBox<String> kategoriField = new ComboBox<>(FXCollections.observableArrayList(
-                "Option 1",
-                "Option 2",
-                "Option 3"
-        ));
-        kategoriField.setPrefWidth(200);
-        VBox kat = new VBox(kategori, kategoriField);
+        categoryField.getItems().add(CustomerType.BASIC);
+        categoryField.getItems().add(CustomerType.MEMBER);
+        categoryField.getItems().add(CustomerType.VIP);
+        categoryField.setPrefWidth(200);
+        kat = new VBox(kategori, categoryField);
 
-        this.list = new VBox(nama.getView(), nomorHandphone.getView(), kat);
+        this.list = new VBox(name.getView(), phoneNumber.getView(), kat);
         this.list.setSpacing(15);
     }
 
