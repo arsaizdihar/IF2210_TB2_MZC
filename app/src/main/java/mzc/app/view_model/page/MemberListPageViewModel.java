05c570ba6c283b40ec5690863a18df61933eb106
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-05-06 03:05:50 +0700

    feat: implement MemberlistPage

--- app/src/main/java/mzc/app/view_model/page/MemberListPageViewModel.java
+++ app/src/main/java/mzc/app/view_model/page/MemberListPageViewModel.java
@@ -1,25 +1,34 @@
 package mzc.app.view_model.page;
 
-import mzc.app.view.components.member_list.LeftSideMemberView;
+import lombok.Getter;
+import mzc.app.adapter.base.IMainAdapter;
+import mzc.app.model.Customer;
+import mzc.app.utils.reactive.State;
+import mzc.app.view.components.member_list.LeftSideMemberListView;
 
 public class MemberListPageViewModel extends SplitPageViewModel {
     public MemberListPageViewModel() {
         super("Daftar Member");
     }
 
+    public static class MemberListContext {
+        @Getter
+        private final State<Customer> customer = new State<>(null);
+        private final IMainAdapter adapter;
+        public MemberListContext(IMainAdapter adapter) { this.adapter = adapter; }
+        public void setCustomer(Customer customer) {
+            this.adapter.getCustomer().persist(customer);
+            this.customer.setValue(customer);
+        }
+    }
     @Override
     public void init() {
         super.init();
-        this.setLeft(new LeftSideMemberView());
+        this.setLeft(new LeftSideMemberListView());
     }
 
     @Override
-    public void onTabFocus() {
-
-    }
-
+    public void onTabFocus() { }
     @Override
-    public void onTabClose() {
-
-    }
+    public void onTabClose() { }
 }
