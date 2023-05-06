75890721e4f6f050fe2e71d0a59fbf651403efe5
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-05-06 16:26:54 +0700

    refactor: MemberView

--- app/src/main/java/mzc/app/view_model/components/member_list/MemberViewModel.java
+++ app/src/main/java/mzc/app/view_model/components/member_list/MemberViewModel.java
@@ -5,13 +5,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import lombok.Getter;
 import lombok.Setter;
 import mzc.app.model.Customer;
 import mzc.app.view_model.base.BaseViewModel;
-import mzc.app.view_model.page.MemberListPageViewModel;
 
 import java.util.Objects;
 
@@ -20,37 +22,67 @@ public class MemberViewModel extends BaseViewModel {
     private Customer customer;
     @Getter private final ImageView avatar = new ImageView(Objects.requireNonNull(getClass().getResource("/mzc/app/assets/avatar.png")).toExternalForm());
     @Getter private HBox memberBox = new HBox();
+
     @Getter private VBox memberInfo = new VBox();
+    @Getter private Text customerName = new Text();
+    @Getter private Label customerType = new Label();
+    @Getter private Label customerPhone = new Label();
+    @Getter private Label customerPoints = new Label();
+    @Getter private HBox wrapperButton = new HBox();
     @Getter private Button transactionButton = new Button("Transaction");
     @Getter private Button editButton = new Button("Edit");
-//    @Getter private Button setActiveButton = new Button(customer.isDeactivated() ? "Activate" : "Deactivate");
+    @Getter private Button setActiveButton = new Button();
     @Override
     public void init() {
-        // TODO : set binding
+        // TODO : set binding and context
         super.init();
-        avatar.setFitWidth(50);
-        avatar.setFitHeight(50);
-        memberBox.setPrefHeight(30);
-        memberBox.setPrefWidth(100);
-        memberBox.setStyle("-fx-border-color: #000000; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-background-color: #ffffff; -fx-background-radius: 5px;");
+        reload();
+    }
+    public void reload() {
+        memberBox.setSpacing(10);
+        setActiveButton.setOnAction(
+                event -> {
+                    if (customer.isDeactivated()) {
+                        customer.setDeactivated(false);
+                        getAdapter().getCustomer().persist(customer);
+                        setActiveButton.setText("Deactivate");
+                    } else {
+                        customer.setDeactivated(true);
+                        getAdapter().getCustomer().persist(customer);
+                        setActiveButton.setText("Activate");
+                    }
+                }
+        );
+        avatar.setFitWidth(64);
+        avatar.setFitHeight(64);
+        memberInfo.setPrefWidth(435);
 
-        memberInfo.setPrefHeight(30);
-        memberInfo.setPrefWidth(50);
-        memberInfo.setStyle("-fx-border-color: #000000; -fx-border-width: 1px; -fx-border-radius: 5px; -fx-background-color: #ffffff; -fx-background-radius: 5px;");
-        memberInfo.getChildren().add(new Label(customer.getName()));
-        memberInfo.getChildren().add(new Label(customer.getType().toString() + (customer.isDeactivated() ? " (deactivated)" : "")));
-        Separator infoSeparator = new Separator(Orientation.HORIZONTAL);
-        infoSeparator.setBackground(null);
-        infoSeparator.setPrefHeight(5);
+
+        customerName.setText(customer.getName());
+        customerName.setWrappingWidth(435);
+        memberInfo.getChildren().add(customerName);
+
+        customerType.setText(customer.getType().toString() + (customer.isDeactivated() ? " (deactivated)" : ""));
+        memberInfo.getChildren().add(customerType);
+
+        HBox infoSeparator = new HBox();
+        infoSeparator.setBackground(Background.fill(Color.TRANSPARENT));
+        infoSeparator.setPrefHeight(2);
         memberInfo.getChildren().add(infoSeparator);
+
         // TODO : tambah icon phone
-        memberInfo.getChildren().add(new Label(customer.getPhone()));
-        memberInfo.getChildren().add(new Label("Points " + customer.getPoints()));
+        customerPhone.setText(customer.getPhone());
+        memberInfo.getChildren().add(customerPhone);
+
+        customerPoints.setText("Points " + customer.getPoints());
+        memberInfo.getChildren().add(new Label(customerPoints.getText()));
 
         memberBox.getChildren().add(avatar);
         memberBox.getChildren().add(memberInfo);
-        memberBox.getChildren().add(transactionButton);
-        memberBox.getChildren().add(editButton);
-//        memberBox.getChildren().add(setActiveButton);
+
+        wrapperButton.getChildren().add(transactionButton);
+        wrapperButton.getChildren().add(editButton);
+        wrapperButton.getChildren().add(setActiveButton);
+        memberBox.getChildren().add(wrapperButton);
     }
 }
