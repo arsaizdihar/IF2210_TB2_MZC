f3ba5a67ebfc0b75a5b9e78ab746fbf03f5be33a
arsaizdihar <arsadihar@gmail.com>
2023-05-06 16:38:23 +0700

    Make app close on some settings changes

--- app/src/main/java/mzc/app/view_model/components/settings/SettingsTabViewModel.java
+++ app/src/main/java/mzc/app/view_model/components/settings/SettingsTabViewModel.java
@@ -2,6 +2,7 @@ package mzc.app.view_model.components.settings;
 
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.stage.Stage;
 import lombok.Getter;
 import mzc.app.view_model.base.BaseViewModel;
 import org.jetbrains.annotations.NotNull;
@@ -52,6 +53,22 @@ public abstract class SettingsTabViewModel extends BaseViewModel {
         var row = new RowConstraints();
         row.setPercentHeight(100);
         root.getRowConstraints().add(row);
+    }
+
+    public void showInfoAlert(boolean needRestart) {
+        var alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Informasi");
+        alert.setHeaderText("Perubahan Berhasil Disimpan");
+        String content = "Perubahan berhasil disimpan.";
+        if (needRestart) {
+            content += " Aplikasi akan dimatikan untuk menerapkan perubahan.";
+        }
+        alert.setContentText(content);
+        alert.showAndWait();
+        if (needRestart) {
+            Stage stage = (Stage) root.getScene().getWindow();
+            stage.close();
+        }
     }
 
     public SettingsTabViewModel() { }
